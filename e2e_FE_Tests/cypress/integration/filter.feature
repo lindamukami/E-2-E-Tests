5cdc68cfc25f3850b256588706d6bf13898df0c1
Linda Mukami <lindamukami32@gmail.com>
2022-10-26 08:32:52 +0300

    filter feature

--- e2e_FE_Tests/cypress/integration/filter.feature
+++ e2e_FE_Tests/cypress/integration/filter.feature
@@ -3,5 +3,10 @@ Feature This feature is required by user to filter a product catalogue
     Scenario: Simple product filter by product Electronics
     Given a user is on the products page
     When they click product Electronics
-    Then they only see a list of Electronic products "WD 2TB Elements Portable External Hard Drive - USB 3.0"
+    Then they only see Electronic item "WD 2TB Elements Portable External Hard Drive - USB 3.0"
+
+    Scenario: Simple product filter by product Electronics
+    Given a user is on the products page
+    When they click product Electronics
+    Then they do not see Fashion item "Mens Cotton Jacket"
 
--- e2e_FE_Tests/cypress/integration/filter/filter.js
+++ e2e_FE_Tests/cypress/integration/filter/filter.js
@@ -2,16 +2,21 @@ import {Given, When, And, Then} from "cypress-cucumber-preprocessor/steps"
 import * as selectors from '../../Selectors'
 
 Given('a user is on the products page', () =>{
-    cy.visit('/')
+    cy.visit('/products')
 })
 
 When('they click product Electronics', () =>{
     cy.get(selectors.QE_TEST_PAGES.FILTER_TEST.PRODUCT_ELECTRONIC)
     .click()
-    cy.wait(2000)
+    .wait(2000)
 })
 
-Then ('they only see a list of Electronic products {string}', (item) =>{
+Then ('they only see Electronic item {string}', (product) =>{
     cy.get(selectors.QE_TEST_PAGES.FILTER_TEST.ITEM_ELECTRONIC)
-    .should("include.text", item)
+    .should("include.text", product)
+    .wait(2000)
+})
+Then ('they do not see Fashion item {string}', (product) =>{
+    cy.get(selectors.QE_TEST_PAGES.FILTER_TEST.ITEM_ELECTRONIC)
+    .should("not.include.text", product)
 })
